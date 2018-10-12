9fb2df924d24b61c8552b8361b5d3213f521ad1a
haroldwongms <harold.wong@microsoft.com>
2018-10-12 16:44:31 -0700

    error tracing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,8 @@
 
 export connstring="$1"
 
+echo $1
+
 docker run --name webui -d -p 8080:80 davevoyles/cash-forecasting-ui
 docker run --name webcontrol -d -p 5001:80 -e Values__ConnString='$connstring' davevoyles/cash-forecasting-api
 docker run --name cashforecasting -d -p 5000:5000 davevoyles/cash-forecasting-pred
