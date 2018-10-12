8edbb8fe2916cfaaec853ad77d585130027c5f1d
haroldwongms <harold.wong@microsoft.com>
2018-10-12 15:31:34 -0700

    update script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@
 
 export connstring="$1"
 
-docker run --name webui -d -p 8080:80 davevoyles/docker-webui
-docker run --name webcontrol -d -p 5001:80 -e Values__ConnString='$connstring' davevoyles/webcontrol
-docker run --name cashforecasting -d -p 5000:5000 davevoyles/docker-cash-forecasting-pred
+docker run --name webui -d -p 8080:80 davevoyles/cash-forecasting-ui
+docker run --name webcontrol -d -p 5001:80 -e Values__ConnString='$connstring' davevoyles/cash-forecasting-api
+docker run --name cashforecasting -d -p 5000:5000 davevoyles/cash-forecasting-pred
